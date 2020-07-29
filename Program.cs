b19aebaec06836bb30c4bab5d158b03c1a9dc80a
Pawel <you@example.com>
2020-07-29 21:47:40 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tydzien4ZadanieDodatkoweNr4
+{
+    class Program
+    {
+        public static string NoPolishSigns(string value)
+        {
+            value = value.Replace("ó", "o")
+            .Replace("ą", "a")
+            .Replace("ę", "e")
+            .Replace("ł", "l")
+            .Replace("ż", "z")
+            .Replace("ź", "z")
+            .Replace("ć", "c")
+            .Replace("ń", "n");
+
+            return value;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Podaj zdanie zawierające polskie znaki : ");
+            string value = Console.ReadLine();
+            Console.WriteLine(NoPolishSigns(value));
+
+        }
+    }
+}
